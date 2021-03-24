62a2da43ae7a3ebc93b35579d71e38755ff569ac
Gökhan SÖNMEZ <gsonmez828@aof.anadolu.edu.tr>
2021-03-24 23:59:10 +0300

    24.03

--- Core/Utilities/Security/Hashing/HashingHelper.cs
+++ Core/Utilities/Security/Hashing/HashingHelper.cs
@@ -13,14 +13,13 @@ namespace Core.Utilities.Security.Hashing
                 passwordSalt = hmac.Key;
                 passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
             }
-
         }
+
         public static bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
         {
             using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
             {
-                var computedHash = passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
-
+                var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
                 for (int i = 0; i < computedHash.Length; i++)
                 {
                     if (computedHash[i] != passwordHash[i])
@@ -29,6 +28,7 @@ namespace Core.Utilities.Security.Hashing
                     }
                 }
             }
+
             return true;
         }
     }
